643eb54d0c9d342fd71ee4e53c3dabd2d4db5fc9
nguyenvu37 <nguyenanhvu.bkdn@gmail.com>
2021-09-29 11:19:43 +0700

    Keep calm and commit

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars({ extname: '.hbs' }));
-        app.set('view engine', 'hbs');
-    app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
 
 // Route init
 route(app);
